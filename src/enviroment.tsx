21829323cc42550baf501ff78c6506bb9c458b77
Hady Muhammed <hadyymohammed@gmail.com>
2024-04-12 23:47:17 +0200

    fixed the api url

--- src/enviroment.tsx
+++ src/enviroment.tsx
@@ -1,4 +1,4 @@
 export const enviroment = {
-  // API_URL: "https://fitscape-backend.onrender.com",
-  API_URL: "http://localhost:1234",
+  API_URL: "https://fitscape-backend.onrender.com",
+  // API_URL: "http://localhost:1234",
 };
